0c4b5fb9539182c71abd234117d6bc3b67a27075
Zonaa2 <ezeaniarinzepaul@gmail.com>
2022-11-04 23:46:28 +0100

    Add files via upload

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,39 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * read_textfile - reads a textfile
- * @filename: file to read
- * @letters: amount of bytes to read
- * Return: printed letters count
+ * read_textfile - function reads a text file and prints it to standard output
+ * @filename: file
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, 0 if fail
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, printed, wrote;
-	char *buff;
+	int fd, n_read, n_write;
+	char *s;
 
-	buff = malloc(letters);
-	if (buff == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+	s = malloc(letters * sizeof(char) + 1);
+	if (!s)
+		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(s);
 		return (0);
-	printed = read(fd, buff, letters);
-	if (printed == -1)
+	}
+	n_read = read(fd, s, letters);
+	if (n_read == -1)
+	{
+		free(s);
 		return (0);
-	wrote = write(STDOUT_FILENO, buff, printed);
-	if (wrote == -1)
-		return (0);
-	if (close(fd) == -1)
-		return (0);
-	free(buff);
-	return (printed);
+	}
+	n_write = write(STDOUT_FILENO, s, n_read);
+	close(fd);
+	free(s);
+	if (n_read == n_write)
+		return (n_write);
+	return (0);
 }
